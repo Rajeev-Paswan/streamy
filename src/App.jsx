5398684fe404753c32dfbda0bbd60693f038e413
Soaib Haque <72245160+SoaibHaque@users.noreply.github.com>
2023-08-19 10:13:05 +0530

    Correcting the routes code formate

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          {/* <Route path="/home" element={<Home/>} /> */}
-          <Route path="/search" element={<Search />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <Routes>
+      <Route index element={<Home />} />
+      {/* <Route path="/home" element={<Home/>} /> */}
+      <Route path="/search" element={<Search />} />
+    </Routes>
   );
 }
 
