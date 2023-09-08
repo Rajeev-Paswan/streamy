332b3d485ead89f70c2eb8d44487207dbd7c3de6
GodRize87 <69663086+GodRize87@users.noreply.github.com>
2023-09-08 20:50:10 +0530

    video source update

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -6,7 +6,7 @@ const Video = () => {
 
   return (
     <div className="video_container">
-        <div className="video_box"><iframe src={"https://vidsrc.me/embed/movie?tmdb="+ id} frameBorder={0} allowFullScreen></iframe></div>
+        <div className="video_box"><iframe src={"https://vidsrc.to/embed/movie/"+ id} frameBorder={0} allowFullScreen></iframe></div>
     </div>
   )
 }
