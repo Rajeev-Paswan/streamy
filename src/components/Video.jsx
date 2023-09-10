a439b1d70c3fac7906dc7a0e417594edf0e447b9
GodRize87 <69663086+GodRize87@users.noreply.github.com>
2023-09-10 13:05:32 +0530

    video source update

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -6,7 +6,7 @@ const Video = () => {
 
   return (
     <div className="video_container">
-        <div className="video_box"><iframe src={"https://vidsrc.to/embed/movie/"+ id} frameBorder={0} allowFullScreen></iframe></div>
+        <div className="video_box"><iframe src={"https://embed.smashystream.com/playere.php?tmdb="+ id} frameBorder={0} allowFullScreen></iframe></div>
     </div>
   )
 }
