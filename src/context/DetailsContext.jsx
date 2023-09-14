6c6d62f638b1ba4c6d9920f74f55b83afd5e12c3
GodRize87 <opkanekikun@gmail.com>
2023-09-14 20:01:39 +0530

    env setup

--- src/context/DetailsContext.jsx
+++ src/context/DetailsContext.jsx
@@ -7,7 +7,8 @@ const DetailsContext = createContext();
 export function DetailsProvider({ children }) {
   const [movieId, setMovieId] = useState("");
   const [details, setDetails] = useState([]);
-  const API_KEY = `88b693131fd0d28f6ad3a9be6b2e10b8`;
+
+  const apiKey = import.meta.env.VITE_API_KEY;
   const API_URL = `https://api.themoviedb.org/3/movie/`;
 
   const updateId = (newId) => {
@@ -19,7 +20,7 @@ export function DetailsProvider({ children }) {
     if (movieId == "") {
       return;
     } else {
-      fetch(`${API_URL}${movieId}?api_key=${API_KEY}`)
+      fetch(`${API_URL}${movieId}?api_key=${apiKey}`)
         .then((response) => response.json())
         .then((data) => {
           setDetails(data);
